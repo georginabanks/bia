57f778b82ed0b5f7da8b2d67cf61dc27191aa72e
Georgina Banks <georginabanks01@gmail.com>
2023-02-22 22:50:08 +0000

    initial commit

---
+++ logic.py
@@ -0,0 +1,422 @@
+# imports
+import sqlite3
+from datetime import datetime
+from datetime import timedelta
+import math
+
+# connect database
+db = sqlite3.connect('data/bia')
+cursor = db.cursor()
+
+# create table for reaction data
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS intervals(
+    dt1 DATETIME UNIQUE NOT NULL,
+    dt2 DATETIME NOT NULL,
+    interval TIME,
+    type TEXT,
+    epi TEXT,
+    CONSTRAINT pk_intervals PRIMARY KEY (dt1,dt2))
+""")
+db.commit()
+
+
+# Reaction class
+class Reaction:
+
+    # init
+    def __init__(self, dt1, dt2, rtype, epi):
+        self.dt1 = dt1
+        self.dt2 = dt2
+        self.rtype = rtype
+        self.epi = epi
+
+    # calculate reaction interval
+    def interval(self):
+        dt_format = '%Y-%m-%d %H:%M'
+        start = datetime.strptime(self.dt1, dt_format)
+        end = datetime.strptime(self.dt2, dt_format)
+
+        total_seconds = (end - start).seconds
+        total_minutes = math.floor(total_seconds / 60)
+
+        hours = math.floor(total_minutes / 60)
+        minutes = total_minutes % 60
+
+        interval = f'{hours}:{minutes}'
+
+        return interval
+
+    # print string
+    def __str__(self):
+        interval = self.interval()
+
+        output = ''
+        output += f'First reaction: {self.dt1}\n'
+        output += f'Second reaction: {self.dt2}\n'
+        output += f'Interval: {interval}\n'
+        output += f'Type: {self.rtype}\n'
+        output += f'Adrenaline: {self.epi}\n'
+
+        print(output)
+
+
+# calculate longest interval
+def longest(rtype, epi):
+
+    try:
+        # read db
+        cursor.execute("""
+            SELECT interval
+            FROM intervals
+            WHERE type=? AND epi=?""",
+            [rtype, epi])
+        li_intervals = cursor.fetchall()
+
+        # error handling for empty list
+        if len(li_intervals) > 0:
+            i_longest = max(li_intervals)
+        else:
+            i_longest = 'No data available.'
+
+        return i_longest
+
+    except Exception as e:
+        raise e
+
+
+# calculate shortest interval
+def shortest(rtype, epi):
+
+    try:
+        # read db
+        cursor.execute("""
+            SELECT interval
+            FROM intervals
+            WHERE type=? AND epi=?""",
+            [rtype, epi])
+        li_intervals = cursor.fetchall()
+
+        # error handling for empty list
+        if len(li_intervals) > 0:
+            i_shortest = min(li_intervals)
+        else:
+            i_shortest = 'No data available.'
+
+        return i_shortest
+
+    except Exception as e:
+        raise e
+
+
+# calculate average
+def calc_average(rtype, epi):
+
+    try:
+        # read db
+        cursor.execute("""
+            SELECT interval
+            FROM intervals
+            WHERE type=? AND epi=?""",
+            [rtype, epi])
+        li_intervals = cursor.fetchall()
+
+        # calculate total time in minutes and no. of reactions
+        tot_minutes = 0
+        reactions = 0
+        for i in li_intervals:
+            m = datetime.strptime(i, '%Y-%m-%d %H:%M')
+            tot_minutes += m
+            reactions += 1
+
+        # average interval in minutes
+        try:
+            avg_minutes = tot_minutes / reactions
+
+        except ZeroDivisionError:
+            avg_minutes = 0
+
+        hours = math.floor(avg_minutes / 60)
+        minutes = tot_minutes % 60
+
+        # avg interval in hours and minutes
+        avg_interval = f'{hours}:{minutes}'
+
+        return avg_interval
+
+    except Exception as e:
+        raise e
+
+
+# welcome message and stats
+def welcome():
+
+    # stats
+    total_average = calc_average('*', '*')
+    longest_total = longest('*', '*')
+    shortest_total = shortest('*', '*')
+
+    if total_average == '0:0':
+        total_average = 'No data available.'
+
+    # output
+    welcome_output = ''
+    welcome_output += 'Welcome!\n'
+    welcome_output += '\n'
+    welcome_output += 'Quick stats:\n'
+    welcome_output += f'Average interval: {total_average}\n'
+    welcome_output += f'Longest interval: {longest_total}\n'
+    welcome_output += f'Shortest interval: {shortest_total}\n'
+
+    print(welcome_output)
+
+
+# add reaction
+def add_reaction():
+
+    # user inputs dates and times of reactions, reaction type, and treatment
+    d1 = input('Date (YYYY-MM-DD): ')
+    t1 = input('Time (HH:MM): ')
+    d2 = input('Date (YYYY-MM-DD): ')
+    t2 = input('Time (HH:MM): ')
+    rtype = input('Anaphylaxis or asthma? ')
+    epi = input('Adrenaline? (Yes/No): ')
+
+    # create dt1 and dt2 from inputs
+    dt1 = datetime.strptime(f'{d1} {t1}', '%Y-%m-%d %H:%M')
+    dt2 = datetime.strptime(f'{d2} {t2}', '%Y-%m-%d %H:%M')
+
+    # calculate interval
+    reaction = Reaction(dt1, dt2, rtype, epi)
+    interval = reaction.interval()
+
+    # insert interval into db
+    try:
+
+        cursor.execute("""
+            INSERT INTO intervals(dt1, dt2, interval, type, epi)
+            VALUES (?,?,?,?,?)""",
+        [dt1, dt2, interval, rtype, epi])
+        db.commit()
+
+        # print interval
+        interval_split = interval.split(':')
+        hours = interval_split[0]
+        minutes = interval_split[1]
+
+        success = f'Interval: {hours} hours {minutes} minutes'
+        print(success)
+
+    # error handling
+    except Exception as e:
+        raise e
+
+    # return to menu
+    finally:
+        return
+
+
+# edit reaction data
+def edit():
+    edit_options = input("""What would you like to edit?
+1. Second reaction date or time
+2. Type of reaction
+3. Treatment with adrenaline
+""")
+
+    try:
+
+        # edit second reaction date and time
+        if edit_options == '1':
+
+            d2 = input('Date  (YYYY-MM-DD): ')
+            t2 = input('Time (HH:MM): ')
+            dt2 = f'{d2}:{t2}'
+
+            # insert into db
+            cursor.execute("""
+                INSERT INTO intervals(dt2)
+                VALUES (?)""",
+                [dt2])
+            db.commit()
+
+        # edit type of reaction
+        if edit_options == '2':
+
+            rtype = input('Type: ')
+
+            # insert into db
+            cursor.execute("""
+                INSERT INTO intervals(type)
+                VALUES (?)""",
+                [rtype])
+            db.commit()
+
+        # edit treatment
+        if edit_options == '3':
+
+            epi = input('Was this reaction treated with adrenaline? ')
+
+            # insert into db
+            cursor.execute("""
+                INSERT INTO intervals(epi)
+                VALUES (?)""",
+                [epi])
+            db.commit()
+
+    except Exception as e:
+        raise e
+
+    finally:
+        return
+
+
+# edit reaction initial menu
+def edit_reaction():
+
+    know_prev = input('Do you know the date and time of the first reaction? ')
+
+    if know_prev.lower() == 'no':
+        view_reactions()
+        edit()
+
+    if know_prev.lower() == 'yes':
+        edit()
+
+
+# view reactions
+def view_reactions():
+    try:
+        # insert into db
+        cursor.execute("""
+            SELECT *
+            FROM intervals(dt1, dt2, type, epi)""")
+        li_reactions = cursor.fetchall()
+
+        # print reactions
+        for row in li_reactions:
+            print(Reaction(row))
+
+    except Exception as e:
+        raise e
+
+    finally:
+        return
+
+
+# view averages
+def view_averages():
+
+    # calculate averages
+    avg_total = calc_average('*', '*')
+    avg_anaphylaxis = calc_average('anaphylaxis', '*')
+    avg_asthma = calc_average('asthma', '*')
+    avg_epi = calc_average('*', 'Yes')
+    avg_no_epi = calc_average('*', 'No')
+
+    # output
+    avg_output = ''
+    avg_output += f'Total: {avg_total}\n'
+    avg_output += f'Anaphylaxis: {avg_anaphylaxis}\n'
+    avg_output += f'Asthma: {avg_asthma}\n'
+    avg_output += f'With adrenaline: {avg_epi}\n'
+    avg_output += f'Without adrenaline: {avg_no_epi}\n'
+
+    print(avg_output)
+
+
+# view stats
+def view_stats():
+
+    # total
+    avg_total = calc_average('*', '*')
+    longest_total = longest('*', '*')
+    shortest_total = shortest('*', '*')
+
+    # anapylaxis
+    avg_anaphylaxis = calc_average('anaphylaxis', '*')
+    longest_anaphylaxis = longest('anaphylaxis', '*')
+    shortest_anaphylaxis = shortest('anaphylaxis', '*')
+
+    # asthma
+    avg_asthma = calc_average('asthma', '*')
+    longest_asthma = longest('asthma', '*')
+    shortest_asthma = shortest('asthma', '*')
+
+    # with adrenaline
+    avg_epi = calc_average('*', 'Y')
+    longest_epi = longest('*', 'Y')
+    shortest_epi = shortest('*', 'Y')
+
+    # without adrenaline
+    avg_no_epi = calc_average('*', 'N')
+    longest_no_epi = longest('*', 'N')
+    shortest_no_epi = shortest('*', 'N')
+
+    # output
+    stats_output = ''
+    stats_output += f'Average: {avg_total}\n'
+    stats_output += f'Longest: {longest_total}\n'
+    stats_output += f'Shortest: {shortest_total}\n'
+    stats_output += '\n'
+    stats_output += f'Average anaphylaxis: {avg_anaphylaxis}\n'
+    stats_output += f'Longest anaphylaxis: {longest_anaphylaxis}\n'
+    stats_output += f'Shortest anaphylaxis: {shortest_anaphylaxis}\n'
+    stats_output += '\n'
+    stats_output += f'Average asthma: {avg_asthma}\n'
+    stats_output += f'Longest asthma: {longest_asthma}\n'
+    stats_output += f'Shortest asthma: {shortest_asthma}\n'
+    stats_output += '\n'
+    stats_output += f'Average with adrenaline: {avg_epi}\n'
+    stats_output += f'Longest with adrenaline: {longest_epi}\n'
+    stats_output += f'Shortest with adrenaline: {shortest_epi}\n'
+    stats_output += '\n'
+    stats_output += f'Average without adrenaline: {avg_no_epi}\n'
+    stats_output += f'Longest without adrenaline: {longest_no_epi}\n'
+    stats_output += f'Shortest without adrenaline: {shortest_no_epi}\n'
+
+    print(stats_output)
+
+
+# display
+welcome()
+
+while True:
+
+    # menu
+    menu = input("""1. Add new reaction
+2. Edit reaction
+3. View reactions
+4. View averages
+5. View stats
+0. Exit
+""")
+
+    # add new reaction
+    if menu == '1':
+        add_reaction()
+
+    # edit reaction
+    if menu == '2':
+        edit_reaction()
+
+    # view reactions
+    if menu == '3':
+        view_reactions()
+
+    # view averages
+    if menu == '4':
+        view_averages()
+
+    # view stats
+    if menu == '5':
+        view_stats()
+
+    # exit
+    if menu == '0':
+        exit()
+
+    # error handling
+    else:
+        print('Something went wrong.\nPlease try again.')
+        continue
